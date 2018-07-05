d63b75da89177cf158b3d911af4fcc5364b511fe
Arenuzzz <arenuzzz@mail.ru>
2018-07-05 23:17:39 +0300

    Confugured project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import { Provider } from "react-redux";
+import { Router, Route, Redirect, browserHistory } from "react-router";
+
+import App from "./components/App.jsx";
+import StartPage from "./components/StartPage.jsx";
+
+import store from "./store";
+
+import "normalize.css";
+import "./assets/main.css";
+
+const routes = (
+  <Route component={App}>
+    <Redirect from="/" to="start" />
+    <Route path="start" component={StartPage} />
+  </Route>
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>{routes}</Router>
+  </Provider>,
+  document.getElementById("root")
+);
